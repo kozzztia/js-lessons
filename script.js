7ae348b9cb3846165eb7c761c0cf466a175cad38
Kostia <kozzztia@gmail.com>
2026-05-30 22:58:52 +0300

    codewar js 5  task

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function solution(str){
-    return str.split('').reverse().join('');
+
+function reverseString(str) {
+    str.split('');
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str.charAt(i);
+    }
+    return result;
+
 }
 
-console.log(solution('lolog'));
+
+console.log(reverseString("Hello, World!"));
