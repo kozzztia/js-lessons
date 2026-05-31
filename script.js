55339020d30e42c039802a5d099d191f396c02b4
Kostia <kozzztia@gmail.com>
2026-05-31 14:07:40 +0300

    codewar js 6  task

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
-function reverseString(str) {
-    str.split('');
-    let result = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        result += str.charAt(i);
-    }
-    return result;
+function test(str) {
+    str = str.toLowerCase();
+    let clean = '';
 
+    for (let i = 0; i < str.length; i++) {
+        // if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9')) {
+        //     clean += str[i];
+        // }
+        if (/[a-z0-9]/.test(str[i])) {
+            clean += str[i];
+        }
+    }
+
+    let reversed = '';
+    for (let i = clean.length - 1; i >= 0; i--) {
+        reversed += clean[i];
+    }
+
+    return clean === reversed;
 }
 
 
-console.log(reverseString("Hello, World!"));
+console.log(test('ollo'));
