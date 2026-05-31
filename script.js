beab4ccc443d3c234fe081fa21a5af08ba3c8f60
Kostia <kozzztia@gmail.com>
2026-05-31 15:18:23 +0300

    codewar js 7  task includes or  new  Set

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
-function test(str) {
-    str = str.toLowerCase().split(/[ \n,;!]/);
+// function test(array) {
+//     let result = []
+//
+//     array.forEach(element => {
+//         if (!result.includes(element)) {
+//             result.push(element)
+//         }
+//     })
+//     console.log(result)
+// }
 
-
-    return str.join('') === str.reverse().join('');
+function test(arr) {
+    console.log([...new Set(arr)]);
 }
 
 
-console.log(test('ollo !')); // true
-console.log(test('Hello'));  // false
-console.log(test('A man, a plan, a canal: Panama'));
+test([1, 2, 2, 3, 4, 4, 5]);
+
+test(["apple", "banana", "apple", "orange", "banana"]);
+
+test([true, true, false, true]);
