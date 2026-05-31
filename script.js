3b2cff4582f7a89ad67e59e3ca65fb9e5603ff67
Kostia <kozzztia@gmail.com>
2026-05-31 23:20:15 +0300

    codewar js 20     const result = Object.keys(counts).reduce((acc, word) => {         return counts[word] > (counts[acc] || 0) ? word : acc;     }, '')

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
 
 function test(str) {
-    const result = [... new Set(str.trim().split(/\s+/))].join(' ')
+    const counts = str.trim().split(/\s+/).reduce((acc, word) => {
+        acc[word] = (acc[word] || 0) +1;
+        return acc;
+    },{})
+
+    const result = Object.keys(counts).reduce((acc, word) => {
+        return counts[word] > (counts[acc] || 0) ? word : acc;
+    }, '')
+
 
     console.log(result);
-    return result;
+    return result
 }
 
 
@@ -21,7 +29,7 @@ function test(str) {
 test("     hello    world hello");
 // // ["level", "madam", "noon", "wow"]
 //
-test("apple banana orange banana apple");
+test("apple banana orange banana");
 // // []
 //
 test("JavaScript is fun fun fun");
