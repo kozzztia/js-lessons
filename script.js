ddcb3af04cfc164eba1250c612c00fc3c420b137
Kostia <kozzztia@gmail.com>
2026-06-02 16:40:03 +0300

    codewar js 26  debounce v2

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const searchInput = document.querySelector('.search');
 
-searchInput.addEventListener(
-    'input',
-    debounce((e) => {
-        console.log('value', e.target.value);
-    }, 500)
-);
+const container = document.querySelector('.container');
+const search = container.querySelector('.search');
+const def = container.querySelector('#default');
+const deb = container.querySelector('#debounce');
+const thr = container.querySelector('#throttle');
 
-function debounce(handler, delay) {
+
+
+// use
+search.addEventListener('input', e => {
+    updateText(def, e.target.value);
+    updateDebText(deb, e.target.value);
+
+})
+
+// function
+
+const updateText = (el, text) =>{
+    el.textContent = text;
+    console.log(text);
+}
+
+const updateDebText = debounce( updateText, 500)
+
+// const updateDebText = debounce( (el, text)=>updateText(el, text), 500)
+
+
+function debounce(callBack, delay = 1000) {
     let timeout;
-
-    return function (...args) {
+    return (...args) => {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            handler.apply(this, args);
+            // callBack.apply(null,args);
+            callBack(...args);
         }, delay);
     };
 }
