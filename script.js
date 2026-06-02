ec36b4d7b65790fe440dc754098ef348696b99d3
Kostia <kozzztia@gmail.com>
2026-06-02 13:25:18 +0300

    codewar js 26  debounce

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
+const searchInput = document.querySelector('.search');
 
-const  container =  document.querySelector('.container')
-let tabCount = 1;
-let autostart = setInterval(()=>start(), 3000)
+searchInput.addEventListener(
+    'input',
+    debounce((e) => {
+        console.log('value', e.target.value);
+    }, 500)
+);
 
-function start() {
-    (tabCount < container.querySelectorAll('.btn').length) ? tabCount++ : tabCount = 1;
-    test(tabCount)
+function debounce(handler, delay) {
+    let timeout;
+
+    return function (...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            handler.apply(this, args);
+        }, delay);
+    };
 }
-
-container.addEventListener('mousedown', (e)=> {
-    const count = +e.target.getAttribute('data-attr')
-    test(count);
-    tabCount = count;
-})
-container.addEventListener('mouseenter', ()=> clearInterval(autostart))
-container.addEventListener('mouseleave', ()=> autostart = setInterval(()=>start(), 3000))
-function test(attr = 1) {
-    if (!attr) return;
-    const str = String(attr);
-    const buttons = container.querySelectorAll('.btn');
-    const tabs = container.querySelectorAll('.tab');
-
-    for (let i = 0; i < buttons.length; i++) {
-        tabs[i].classList.toggle('active', str === tabs[i].getAttribute('data-attr'));
-        buttons[i].classList.toggle('active', str === buttons[i].getAttribute('data-attr'));
-    }
-}
-// init
-test()
-
